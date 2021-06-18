fef289e462e085c9302bc91104b9fd4f100e3613
ceifa <gabrinelson27@gmail.com>
2021-06-18 01:40:57 -0300

    Add legacy files quantity report

--- lua/downloader/legacy_scanner.lua
+++ lua/downloader/legacy_scanner.lua
@@ -4,22 +4,21 @@ MODULE.Priority = 10
 local resourceExtensions = include("downloader/resources.lua")
 local shouldScan = CreateConVar("downloader_legacy_scan_danger", 0, FCVAR_ARCHIVE, "Should scan for legacy addons (DANGER!!!)")
 
-local function AddFiles(originPath, currentPath)
+local function AddFiles(originPath, currentPath, legacyFiles)
     local files, dirs = file.Find(originPath .. currentPath .. "*", "MOD")
 
     if files then
         for _, subFile in ipairs(files) do
             local ext = string.GetExtensionFromFilename(subFile)
             if resourceExtensions[ext] then
-                print(string.format("[DOWNLOADER] ADDING LEGACY ADDON FILE '%s'", currentPath .. subFile))
-                resource.AddFile(currentPath .. subFile)
+                table.insert(legacyFiles, currentPath .. subFile)
             end
         end
     end
 
     if dirs then
         for _, subDir in ipairs(dirs) do
-            AddFiles(originPath, currentPath .. subDir .. "/")
+            AddFiles(originPath, currentPath .. subDir .. "/", legacyFiles)
         end
     end
 end
@@ -28,16 +27,25 @@ local function ScanAddons()
     local _, folders = file.Find("addons/*", "MOD")
     local currentMap = game.GetMap()
 
+    local legacyFiles = {}
+
     for _, folder in ipairs(folders or {}) do
-        AddFiles("addons/" .. folder .. "/", "")
+        AddFiles("addons/" .. folder .. "/", "", legacyFiles)
 
         local mapFiles = file.Find("addons/" .. folder .. "/maps/*.bsp", "MOD") or {}
         for _, mapFile in ipairs(mapFiles) do
             if string.StripExtension(mapFile) == currentMap then
-                resource.AddFile("maps/" .. mapFile .. ".bsp")
+                table.insert(legacyFiles, "maps/" .. mapFile .. ".bsp")
             end
         end
     end
+
+    for _, legacyFile in ipairs(legacyFiles) do
+        print(string.format("[DOWNLOADER] [+] LEGACY '%s'", legacyFile))
+        resource.AddFile(legacyFile)
+    end
+
+    print("[DOWNLOADER] FINISHED TO ADD LEGACY ADDONS: " .. #legacyFiles .. " FILES SELECTED")
 end
 
 function MODULE:Run(context)
