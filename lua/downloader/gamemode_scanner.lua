11b9ddf60bc70b0704aa65d40d898cf86bba4c22
ceifa <gabrinelson27@gmail.com>
2021-06-22 21:07:15 -0300

    Fix gamemode scanning get wrong addons with gamemode folder

--- lua/downloader/gamemode_scanner.lua
+++ lua/downloader/gamemode_scanner.lua
@@ -3,34 +3,47 @@ MODULE.Order = 2
 
 local currentGamemode = engine.ActiveGamemode()
 
-local function IsUsingGamemode(gamemodeFolders, addonTitle)
-    for _, gamemodeFolder in ipairs(gamemodeFolders) do
-        -- file.Exists does not work here
-        local gamemodeFiles = file.Find("gamemodes/" .. gamemodeFolder .. "/" .. currentGamemode .. ".txt", addonTitle)
+local function IsCurrentGamemode(gamemodeFolders, addonTitle)
+    local totalGamemodeFiles = 0
 
-        if #gamemodeFiles == 1 then
+    for _, gamemodeFolder in ipairs(gamemodeFolders) do
+        if gamemodeFolder == currentGamemode then
             return true
+        end
+
+        -- file.Exists does not work here
+        local gamemodeFiles = file.Find("gamemodes/" .. gamemodeFolder .. "/*.txt", addonTitle)
+
+        totalGamemodeFiles = totalGamemodeFiles + #gamemodeFiles
+
+        for _, gamemodeFile in ipairs(gamemodeFiles) do
+            if gamemodeFile == currentGamemode then
+                return true
+            end
         end
     end
 
-    return false
+    return totalGamemodeFiles == 0 and nil or false
 end
 
 function MODULE:Run(context)
-    local gamemodeFound = false
-
     for _, addon in ipairs(context.addons) do
         -- Does not support wildcard on folders :(
         local _, gamemodeFolders = file.Find("gamemodes/*", addon.title)
 
         if #gamemodeFolders > 0 then
-            if not gamemodeFound and IsUsingGamemode(gamemodeFolders, addon.title) then
-                table.insert(context.usingAddons, addon)
-                gamemodeFound = true
-            end
+            -- true if it's current gamemode
+            -- false if it's a gamemode but not the current
+            -- nil if it's a false positive, probably not a gamemode
+            local isCurrentGamemode = IsCurrentGamemode(gamemodeFolders, addon.title)
+            if isCurrentGamemode ~= nil then
+                if isCurrentGamemode then
+                    table.insert(context.usingAddons, addon)
+                end
 
-            -- Is probably a gamemode addon, resources should be ignored
-            context.ignoreResources[addon.wsid] = true
+                -- Is probably a gamemode addon, resources should be ignored
+                context.ignoreResources[addon.wsid] = true
+            end
         end
     end
 end
